c5d4436f3c6900d0663aff9227f49218ea49db72
qba97 <jakublokiec@wp.pl>
2018-03-09 13:13:34 +0100

    dodano obsługę plików jako argumenty uruchomienia programu

--- Rozgrzewka2/src/Main.java
+++ Rozgrzewka2/src/Main.java
@@ -3,8 +3,9 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	    String fileName = "in3.txt";
-	    InputReader ir = new InputReader(fileName);
+	    String inputFileName = args[0];
+	    String outputFileName = args[1];
+	    InputReader ir = new InputReader(inputFileName);
 	    ir.read();
 	    ir.parseAllLines();
         List<DataLine> dataLines = ir.getDataLines();

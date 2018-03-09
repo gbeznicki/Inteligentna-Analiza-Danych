54fea862d75e61ba6952601a13c5e9e8f692bbc2
qba97 <jakublokiec@wp.pl>
2018-03-09 14:26:14 +0100

    dodano klasę liczącą pochodną sigmoidy

--- Rozgrzewka2/src/program/SigmoidDerivative.java
+++ Rozgrzewka2/src/program/SigmoidDerivative.java
@@ -1,4 +1,14 @@
 package program;
 
 public class SigmoidDerivative {
+
+    private Sigmoid sigmoid;
+
+    public SigmoidDerivative(Sigmoid sigmoid) {
+        this.sigmoid = sigmoid;
+    }
+
+    public double calcSigmoidDerivative() {
+        return sigmoid.calcSigmoidForVector() * (1 - sigmoid.calcSigmoidForVector());
+    }
 }

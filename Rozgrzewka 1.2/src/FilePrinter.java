36742a9a9097a362af4eb47b70f74512954d991c
gbeznicki <g.beznicki@gmail.com>
2018-04-10 17:21:38 +0200

    Created random point generator

---
+++ Rozgrzewka 1.2/src/FilePrinter.java
@@ -0,0 +1,2 @@
+public class FilePrinter {
+}
--- Rozgrzewka 1.2/src/Program.java
+++ Rozgrzewka 1.2/src/Program.java
@@ -1,6 +1,22 @@
+import java.sql.SQLOutput;
+import java.util.List;
+
 public class Program {
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("Hello world!");
+        // TODO 2 okręgi, RPG, file printer, kmeansAlgorithm
+
+        Point centre1 = new Point(-3, 0);
+        Point centre2 = new Point(3, 0);
+
+        int quantity = 10000;
+
+        RandomPointGenerator randomPointGenerator = new RandomPointGenerator(quantity, centre1);
+        List<Point> pointList = randomPointGenerator.generate();
+
+        for(int i =0; i<quantity; i++){
+
+            System.out.println(pointList.get(i).getX()+ " "+ pointList.get(i).getY());
+        }
+
     }
 }

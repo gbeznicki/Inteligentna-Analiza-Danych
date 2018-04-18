3202fb0d74ad40828541c34e28d3ff895c6499c2
qba97 <jakublokiec@wp.pl>
2018-04-18 09:17:58 +0200

    fixed WinnerTakesMost

--- Rozgrzewka 2.2/src/com/company/WinnerTakesMost.java
+++ Rozgrzewka 2.2/src/com/company/WinnerTakesMost.java
@@ -12,13 +12,13 @@ public class WinnerTakesMost implements NeighbourhoodFunction {
         this.radius = radius;
     }
 
-// promien zostaje, distance distance zap omocą odleglosci indeksow output zostaje
+    // promien zostaje, distance distance zap omocą odleglosci indeksow output zostaje
     @Override
-    public double calculateTheta(List<Point> neurons, int winningNeuronIndex,int currentNeuronIndex, int iteration) {
+    public double calculateTheta(List<Point> neurons, int winningNeuronIndex, int currentNeuronIndex, int iteration) {
         // radius(t) = radius(0)*(1/t)
         double sigma = radius / iteration;
         double distance = Math.abs(winningNeuronIndex - currentNeuronIndex);
-        double output = Math.exp(-distance * distance / 2 * sigma * sigma);
+        double output = Math.exp(-(distance * distance) / (2 * sigma * sigma));
 
         return output;
     }

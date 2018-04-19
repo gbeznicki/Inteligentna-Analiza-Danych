1f8f981a7d174a3241ccc5295efd573f7086f8f6
gbeznicki <g.beznicki@gmail.com>
2018-04-19 10:31:02 +0200

    Initial commit

---
+++ Rozgrzewka 2.3/src/com/company/Point.java
@@ -0,0 +1,62 @@
+package com.company;
+
+public class Point {
+    private double x;
+    private double y;
+    private int group;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Point p) {
+        this.x = p.x;
+        this.y = p.y;
+    }
+
+    public Point(double x, double y, int group) {
+        this.x = x;
+        this.y = y;
+        this.group = group;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    @Override
+    public String toString() {
+        return x + " " + y;
+    }
+
+
+    public static double calculateDistanceBetweenPoints(Point p1, Point p2) {
+        double x1 = p1.getX();
+        double x2 = p2.getX();
+        double y1 = p1.getY();
+        double y2 = p2.getY();
+        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    }
+
+}
---
+++ Rozgrzewka 2.3/src/com/company/SOM.java
@@ -0,0 +1,130 @@
+package com.company;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static com.company.Point.calculateDistanceBetweenPoints;
+
+public class SOM {
+
+    private static final double PRECISION = 1e-3;
+    private static final double LEARNING_RATE_RATIO = 0.9;
+
+    private List<Point> dataPoints;
+    private List<Point> neurons;
+    private List<Point> previousNeurons;
+    private double initialLearningRate;
+    private double actualLearningRate;
+    private int iteration = 1;
+    private double actualError;
+
+    public SOM(List<Point> dataPoints, List<Point> neurons, double initialLearningRate) {
+        this.dataPoints = dataPoints;
+        this.neurons = neurons;
+        this.initialLearningRate = initialLearningRate;
+        actualLearningRate = initialLearningRate;
+        this.func = func;
+        previousNeurons = new ArrayList<>();
+        for (int i = 0; i < neurons.size(); i++) {
+            previousNeurons.add(new Point(neurons.get(i)));
+        }
+    }
+
+    public Point getBestMatchingUnit(Point dataPoint) {
+        double min = Double.MAX_VALUE;
+        int index = 0;
+
+        for (int i = 0; i < neurons.size(); i++) {
+            double distance = calculateDistanceBetweenPoints(dataPoint, neurons.get(i));
+            if (distance < min) {
+                min = distance;
+                index = i;
+            }
+        }
+
+        return neurons.get(index);
+    }
+
+    public void updateLearningRate() {
+//        actualLearningRate = initialLearningRate / iteration;
+        actualLearningRate *= LEARNING_RATE_RATIO;
+    }
+
+    public void iterate() {
+        Random random = new Random();
+
+        Point randomDataPoint = dataPoints.get(random.nextInt(dataPoints.size()));
+
+        Point bmu = getBestMatchingUnit(randomDataPoint);
+
+        for (int i = 0; i < neurons.size(); i++) {
+            double x = neurons.get(i).getX();
+            double y = neurons.get(i).getY();
+            x += actualLearningRate * func.calculateTheta(neurons, neurons.indexOf(bmu),i, iteration) * (randomDataPoint.getX() - bmu.getX());
+            y += actualLearningRate * func.calculateTheta(neurons, neurons.indexOf(bmu),i, iteration) * (randomDataPoint.getY() - bmu.getY());
+            neurons.get(i).setX(x);
+            neurons.get(i).setY(y);
+        }
+    }
+
+    public boolean checkStopCondition() {
+
+        for (int i = 0; i < neurons.size(); i++) {
+            double diffX = Math.abs(previousNeurons.get(i).getX() - neurons.get(i).getX());
+            double diffY = Math.abs(previousNeurons.get(i).getY() - neurons.get(i).getY());
+
+            if (diffX > PRECISION || diffY > PRECISION) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void updatePreviousNeurons() {
+        previousNeurons.clear();
+        for (int i = 0; i < neurons.size(); i++) {
+
+            previousNeurons.add(new Point(neurons.get(i)));
+        }
+    }
+
+    public void calculateError() {
+        for (int i = 0; i < dataPoints.size(); i++) {
+            Point dataPoint = dataPoints.get(i);
+            Point closestNeuron = getBestMatchingUnit(dataPoints.get(i));
+            actualError += calculateDistanceBetweenPoints(dataPoint, closestNeuron);
+        }
+        actualError /= dataPoints.size();
+    }
+
+    public void doSOM() {
+        do {
+            actualError = 0;
+            updatePreviousNeurons();
+            iterate();
+            updateLearningRate();
+            calculateError();
+            System.out.println(actualError);
+            saveToFile(iteration);
+
+            iteration++;
+        } while (!checkStopCondition());
+
+        System.out.println(iteration);
+    }
+
+    private void saveToFile(int index) {
+
+        try (PrintWriter printWriter = new PrintWriter("punkty" + index + ".txt")) {
+            for (int i = 0; i < neurons.size(); i++) {
+                printWriter.println(neurons.get(i));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}

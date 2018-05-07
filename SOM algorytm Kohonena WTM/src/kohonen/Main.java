452311b0707281dc93ce9a4a9d2f6b49ca56b56c
gbeznicki <g.beznicki@gmail.com>
2018-05-07 21:29:53 +0200

    Need to check stop condidion in doSOM method

--- SOM algorytm Kohonena WTM/src/kohonen/Main.java
+++ SOM algorytm Kohonena WTM/src/kohonen/Main.java
@@ -1,8 +1,39 @@
 package kohonen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Point centre1 = new Point(-3, 0);
+        Point centre2 = new Point(3, 0);
+
+        int quantity = 200;
+        int k = 10;
+
+        PointFactory pointFactory = new PointFactory();
+        List<Point> dataPoints = new ArrayList<>();
+        List<Neuron> neuronsList = new ArrayList<>();
+
+
+        //Losowanie 1 koła czarnych punktów
+        for (int i = 0; i < quantity; i++) {
+            dataPoints.add(pointFactory.generatePointsInsideCircle(2, centre1));
+        }
+
+        //Losowanie 2 koła czarnych punktów
+        for (int i = 0; i < quantity; i++) {
+            dataPoints.add(pointFactory.generatePointsInsideCircle(2, centre2));
+        }
+
+        //Losowanie neuronow
+        for (int i = 0; i < k; i++) {
+            Neuron neuron = pointFactory.generateRandomNeuron(-10, 10);
+            neuronsList.add(neuron);
+        }
+
+        SOM som = new SOM(dataPoints, neuronsList);
+        som.doSOM();
     }
 }
---
+++ SOM algorytm Kohonena WTM/src/kohonen/Point.java
@@ -0,0 +1,30 @@
+package kohonen;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Point p) {
+        this.x = p.x;
+        this.y = p.y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return x + " " + y;
+    }
+
+}

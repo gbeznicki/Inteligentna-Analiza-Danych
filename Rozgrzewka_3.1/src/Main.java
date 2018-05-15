1941825aab4944377b66126d2d2bd1e5ddf81714
qba97 <jakublokiec@wp.pl>
2018-05-15 17:13:30 +0200

    done and works

--- Rozgrzewka_3.1/src/Main.java
+++ Rozgrzewka_3.1/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    RadialBasisFunction function = new RadialBasisFunction(10);
+	    function.run(5, 15, 0.01);
     }
 }

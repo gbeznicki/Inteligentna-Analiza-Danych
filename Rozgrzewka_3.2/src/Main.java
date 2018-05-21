d68bcd30c536c26dc5347d2382a49e852ce29e39
qba97 <jakublokiec@wp.pl>
2018-05-21 12:05:58 +0200

    dodanie klas RadialBasisFunction i Point z poprzedniej rozgrzewki, zmiana parametrów sieci

--- Rozgrzewka_3.2/src/Main.java
+++ Rozgrzewka_3.2/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    RadialBasisFunction function = new RadialBasisFunction(4);
+	    function.run(-10, 10, 0.01);
     }
 }
